fb9724957a8f9f6a9a7b70f3fb24524a51ded5be
bhaskar_bisht <bisht3182568@gmail.com>
2024-09-12 11:43:19 +0530

    update user controller

--- controllers/userController/user.Controller.js
+++ controllers/userController/user.Controller.js
@@ -80,6 +80,8 @@ export const loginUser = async (req, res, next) => {
         res.cookie("notes_token", token, {
             httpOnly: true,
             secure: true,
+            sameSite: "none",
+
             maxAge: 2 * 24 * 60 * 60 * 1000,
         }).json({
             success: true,
